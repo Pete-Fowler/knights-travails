92a57d3088d148f5708328f62c09a52e42d0096e
Pete Fowler <fowler.pete@gmail.com>
2022-12-02 14:30:55 -0700

    Add validMoves method

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Knight from "./knight.js";
+
+const k = new Knight();
+
+console.log(k.possibleMoves(7, 7));
--- src/knight.js
+++ src/knight.js
@@ -1,1 +1,29 @@
-function knightMoves(from, to) {}
+export default class Knight {
+  constructor() {}
+
+  knightMoves(from, to, next = []) {
+    [x, y] = from;
+    [i, j] = to;
+
+    let a = x;
+    let b = y;
+  }
+
+  possibleMoves(x, y) {
+    const moves = [
+      [x + 2, y + 1],
+      [x + 2, y - 1],
+      [x + 1, y - 2],
+      [x - 1, y - 2],
+      [x - 2, y - 1],
+      [x - 2, y + 1],
+      [x - 1, y + 2],
+      [x + 1, y + 2],
+    ];
+
+    const validMoves = moves.filter(
+      (arr) => arr[0] >= 0 && arr[0] <= 7 && arr[1] >= 0 && arr[1] <= 7
+    );
+    return validMoves;
+  }
+}
