dd1d8614275ce27b2bb7cc477b63c0dfd5893ec5
Pete Fowler <fowler.pete@gmail.com>
2022-12-05 12:30:42 -0700

    Get proper distance, need proper path

--- src/knight.js
+++ src/knight.js
@@ -4,40 +4,25 @@ import _ from "lodash";
 export default class Knight {
   bestPath(from, to, distance = 0, queue = [], visited = []) {
     if (_.isEqual(from.coords || from, to)) {
-      const str = visited.reduce((a, b, i) => {
-        if (i === 0) return a + `[${b}]`;
-        else return a + ` => [${b}]`;
-      });
+      return `The knight made it in ${from.distance} move(s)! \n ${visited.map(
+        (v) => {
+          return `${v.coords}`;
+        }
+      )}`;
     }
+    visited.push(new Node((from.coords || from, distance)));
 
-    visited.push(new Node((from || from.coords, distance)));
+    const next = this.moves(from.coords || from, visited);
 
-    const next = this.moves(from || from.coords, visited);
-
-    queue.push(next.map((move) => new Node(move, distance + 1)));
+    queue.push(...next.map((move) => new Node(move, distance + 1)));
     console.log(queue);
-
     if (queue.length < 1) return;
 
-    return;
-    // this.bestPath(queue.shift(), to, distance + 1, queue);
+    return this.bestPath(queue.shift(), to, distance + 1, queue, visited);
   }
-  /*
-  levelOrder(callback = false, node = this.root, queue = [], visited = []) {
-    if (node === null) return;
 
-    callback ? callback(node) : visited.push(node.data);
-    queue.push(node.left, node.right);
-    const next = queue.shift();
-
-    this.levelOrder(callback, next, queue, visited);
-
-    if (callback) return;
-    return visited;
-  }
-  */
-  moves(coords, visited) {
-    const [x, y] = coords;
+  moves(from, visited) {
+    const [x, y] = from.coords || from;
     const moves = [
       [x + 2, y + 1],
       [x + 2, y - 1],
@@ -54,12 +39,24 @@ export default class Knight {
     );
 
     const validUnvisitedMoves = _.difference(validMoves, visited);
-    // console.log(validUnvisitedMoves);
 
     return validUnvisitedMoves;
   }
 }
+/*
+  levelOrder(callback = false, node = this.root, queue = [], visited = []) {
+    if (node === null) return;
 
+    callback ? callback(node) : visited.push(node.data);
+    queue.push(node.left, node.right);
+    const next = queue.shift();
+
+    this.levelOrder(callback, next, queue, visited);
+
+    if (callback) return;
+    return visited;
+  }
+  */
 /*
 This problem can be seen as the shortest path in an unweighted graph. Therefore we use BFS to solve this problem. 
 
